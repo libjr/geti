b601307d1b58166fec1c2027c795557d7bc74f6e
fus <freemiumuser01@gmail.com>
2024-10-31 16:47:33 +0800

    Update get_code_admin_app.php

--- get_code_admin_app.php
+++ get_code_admin_app.php
@@ -1,7 +1,7 @@
 <?php
 // List of allowed domains
 $allowed_domains = [
-    'https://lax.mhx.mybluehost.me/website_2bb4b188sdsd',
+    'https://lax.mhx.mybluehost.me/website_2bb4b188',
     'anotherexample.com',
     'yetanotherexample.com'
 ];
